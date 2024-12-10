27c4a9bcf4b165a88cddbacce9110b9fc752b755
Enes Uysal <hulkienesuysal@gmail.com>
2024-12-10 13:02:49 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMa.snf.sdlncsadlR
+          Edit <code>src/App.tsx</code> and save to test burasu güngellendi
         </p>
       </div>
       <p className="read-the-docs">
