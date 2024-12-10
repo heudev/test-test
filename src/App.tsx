d47e3d8e24166644ac7725796bb6cb2d3a9fe07d
heudev <hulkienesuysal@gmail.com>
2024-12-10 13:08:10 +0300

    commit 3

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test burasu güngellendi
+          Edit <code>src/App.tsx</code> and save to test HMR commit 3
         </p>
       </div>
       <p className="read-the-docs">
